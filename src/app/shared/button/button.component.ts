fa666332d39a53152397ba189c1f035477dada3c
Francisco Javier Florencio Airado <fjflorenc@gmail.com>
2024-07-28 14:25:39 +0200

    ADVANCED CONTENT PROJECTION - ng-content

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
+  // any button with this atribute selector [appButton] will have 
+  // the styles and template of this component
   selector: 'button[appButton]',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
   styleUrl: './button.component.css'
 })
-export class ButtonComponent {
-
-}
+export class ButtonComponent {}
