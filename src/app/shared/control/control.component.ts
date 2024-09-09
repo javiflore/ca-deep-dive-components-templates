9515dc720d27c04490a0fdf186cce13d021ab78a
Francisco Javier Florencio Airado <fjflorenc@gmail.com>
2024-09-09 13:02:34 +0200

    encapsulating styles

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,15 +1,16 @@
-import { Component, input} from '@angular/core';
+import { Component, ViewEncapsulation, input} from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None // <--- This line disables the style encapsulation, control.component.css will become global.
 })
 export class ControlComponent {
 
   label = input.required<string>();
-  
+
 
 }
