3d593ade0aeb6afde837df8e9d0c317dd5aef0ce
Francisco Javier Florencio Airado <fjflorenc@gmail.com>
2024-09-09 14:23:51 +0200

    @hostListener

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation, input} from '@angular/core';
+import { Component, HostBinding, HostListener, ViewEncapsulation, input} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -7,13 +7,24 @@ import { Component, ViewEncapsulation, input} from '@angular/core';
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None, // <--- This line disables the style encapsulation, control.component.css will become global.
-  host: {
-    class: 'control'
-  }
+  // host: {
+  //   class: 'control',
+  //   '(click)': 'onClick($event)'
+  // }
 })
 export class ControlComponent {
 
+  @HostBinding('class') className = 'control';
+
+  @HostListener('click') onClick2(event: Event) {
+    console.log('Clicked');
+  }
+
   label = input.required<string>();
+
+  onClick(event: Event) {
+    console.log('ControlComponent.onClick', event);
+  }
 
 
 }
