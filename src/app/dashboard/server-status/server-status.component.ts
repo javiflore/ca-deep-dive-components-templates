0d7cd9298fedb543e2f6d7129ba936d9df6d6b8a
Francisco Javier Florencio Airado <fjflorenc@gmail.com>
2024-09-10 09:51:04 +0200

    ngOnInit

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -12,11 +12,18 @@ export class ServerStatusComponent {
   // literal types - only allow specific string values
   currentStatus: 'online'|'offline'|'unknown' = 'unknown';
 
+
   constructor() {
+    
+  }
+
+
+  // ngOnInit runs once the component has initialized all the inputs()
+  ngOnInit() {
     this.currentStatus = Math.random() > 0.5 ? 'online' : 'offline';
 
     setInterval(() => {
       this.currentStatus = Math.random() > 0.5 ? 'online' : 'offline';
-    }, 1000);
+    }, 3000);
   }
 }
